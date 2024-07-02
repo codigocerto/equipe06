6fa6e52eb25e8857023fe143b8faaacf91645309
Raphael Mazzieri <rquitto@gmail.com>
2024-07-02 19:57:52 -0300

    refactor: realizado a refatoração do codigo dos arquivos: footer.css, menu-hamburguer.css, index.html e menu-hamburuger.js

--- frontend/js/menu-hamburguer.js
+++ frontend/js/menu-hamburguer.js
@@ -1,9 +1,14 @@
-const burgerMenu = document.getElementById('burger-menu');
+// Obtém o elemento com o ID 'burger-menu'
+const burgerMenu = document.getElementById("burger-menu");
 
-const overlay = document.getElementById('menu');
+// Obtém o elemento com o ID 'menu'
+const overlay = document.getElementById("menu");
 
-burgerMenu.addEventListener('click', function() {
-    
-  burgerMenu.classList.toggle("close");
-  overlay.classList.toggle("overlay");
+burgerMenu.addEventListener("click", function () {
+ 
+  burgerMenu.classList.toggle("close"); /* Alterna a classe "close" no elemento burgerMenu
+  Se a classe estiver presente, ela será removida; se não estiver, ela será adicionada */
+
+  overlay.classList.toggle("overlay"); /* Alterna a classe "overlay" no elemento overlay
+  Se a classe estiver presente, ela será removida; se não estiver, ela será adicionada */
 });
