75b2a8d5c48073053812c943edb254a11d204284
Dickson Freitas <dickfreitas12@gmaill.com>
2024-07-16 18:34:04 -0300

    feat: Implementação de envio de email

--- backend/src/enrollment/enrollment.module.ts
+++ backend/src/enrollment/enrollment.module.ts
@@ -3,10 +3,11 @@ import { EnrollmentService } from './enrollment.service';
 import { EnrollmentController } from './enrollment.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnrollmentEntity } from './entities/EnrollmentEntity';
+import { SendEmailService } from 'src/send-email/send-email.service';
 
 @Module({
   imports:[TypeOrmModule.forFeature([EnrollmentEntity])],
-  providers: [EnrollmentService],
+  providers: [EnrollmentService , SendEmailService],
   controllers: [EnrollmentController]
 })
 export class EnrollmentModule {}
